af7c04fd7184435305ee6b167adacdbcde960da3
Alexander Hems <Alexander.Hems@t8y.com>
2016-02-22 22:37:46 +0100

    dude

--- red.py
+++ red.py
@@ -1466,8 +1466,12 @@ async def getRandomSpodermanMeme(message):
     content = message.content.split()
     url = "https://api.imgur.com/3/gallery/NcHpG"
     headers = {"Authorization": "Client-ID 52c6e89eac4bb0f"}
-    data = requests.get(url, headers=headers).json
-    number = int(random.random()*229)
+    data = requests.get(url, headers=headers).json()
+    number = 0
+    if len(content) >= 2 :
+        number = int(content[1])
+    else :
+        number = int(random.random()*229)
     link = data['data']['images'][number]['link']
     await client.send_message(message.channel, link.format(id, message.author.name))
 
